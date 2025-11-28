3b9ba7faa1e5b2811b5765f1532821fce6d19233
alwanalfians <alwanalfians4@gmail.com>
2025-11-28 16:48:25 +0700

    feat(setup): setup base code structure

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: `${process.env.title}`,
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
